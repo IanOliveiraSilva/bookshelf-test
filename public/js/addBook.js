5ea52f77254380530b0bcbf28b0055b7880ff3df
ianoliveirasilva <ianoliveira.tec@gmail.com>
2024-05-08 23:15:57 -0300

    Adding error message when submit without file in add book page

--- public/js/addBook.js
+++ public/js/addBook.js
@@ -1,28 +1,42 @@
-document.getElementById('add-book-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', async () => {
+    const submitButton = document.getElementById('submit-button') 
+    
+    document.getElementById('add-book-form').addEventListener('submit', async (event) => {
+        event.preventDefault();
+    
+        if (!document.getElementById('image').files.length) {
+            alert('Por favor, selecione um arquivo antes de enviar.');
+            return;
+        }
+    
+        const formData = new FormData();
+        formData.append('name', document.getElementById('name').value);
+        formData.append('description', document.getElementById('description').value);
+        formData.append('author', document.getElementById('author').value);
+        formData.append('year', document.getElementById('year').value);
+        formData.append('publisher', document.getElementById('publisher').value);
+        formData.append('image', document.getElementById('image').files[0]);
+    
+        submitButton.disabled = true;
 
-    const formData = new FormData();
-    formData.append('name', document.getElementById('name').value);
-    formData.append('description', document.getElementById('description').value);
-    formData.append('author', document.getElementById('author').value);
-    formData.append('year', document.getElementById('year').value);
-    formData.append('publisher', document.getElementById('publisher').value);
-    formData.append('image', document.getElementById('image').files[0]);
-
-    const response = await fetch('/api/book/', {
-        method: 'POST',
-        body: formData
+        const response = await fetch('/api/book/', {
+            method: 'POST',
+            body: formData
+        });
+    
+        if (response.ok) {
+            alert('Livro adicionado com sucesso!');
+            window.location.href = `/bookshelf`;
+        } else {
+            alert('Falha ao adicionar livro');
+        }
     });
+    
+    document.getElementById('image').addEventListener('change', function() {
+        document.getElementById('file-name').textContent = this.files[0].name;
+    });
+})
 
-    if (response.ok) {
-        alert('Livro adicionado com sucesso!');
-        window.location.href = `/bookshelf`;
-    } else {
-        alert('Failed to add book');
-    }
-});
 
-document.getElementById('image').addEventListener('change', function() {
-    document.getElementById('file-name').textContent = this.files[0].name;
-});
+
 
